d37e5f4bb86d7362d5b64b6e4c204d17a6209bf1
“AgnesSzigethy” <szigethy.agnes@gmail.com>
2017-03-11 22:51:33 -0800

    creating the fibonacci sequence

--- Fibonacci/source/Fibonacci.js
+++ Fibonacci/source/Fibonacci.js
@@ -10,7 +10,19 @@ class Fibonacci {
 
 		return this.getFibonacciNumber(index - 1) + this.getFibonacciNumber(index - 2);
 	}
+
+	getFibonacciSequence(n) {
+		let fibonacciSequence = [];
+		for (let i = 0; i < n; i++) {
+			fibonacciSequence.push(this.getFibonacciNumber(i));
+		}
+
+		return fibonacciSequence;
+	}
 }
+
+let fib = new Fibonacci();
+fib.getFibonacciSequence(10);
 
 export default Fibonacci;
 
