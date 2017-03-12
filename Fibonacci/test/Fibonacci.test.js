edb621ada9d5698f88674afe4e7ce336a9a7df9d
“AgnesSzigethy” <szigethy.agnes@gmail.com>
2017-03-11 22:18:59 -0800

    adding test

--- Fibonacci/test/Fibonacci.test.js
+++ Fibonacci/test/Fibonacci.test.js
@@ -11,7 +11,17 @@ describe("Fibonacci", () => {
     it("shall have the ability to get the fibonacci number by index", () => {
         let fibonacci = new Fibonacci();
 
-	expect(fibonacci.getFibonacciNumber(0)).to.equal(0);
+		expect(fibonacci.getFibonacciNumber(0)).to.equal(0);
+    });
+
+    it("shall return the proper indexed Fibonacci number", () => {
+    	let fibonacci = new Fibonacci();
+
+    	expect(fibonacci.getFibonacciNumber(0).to.equal(0));
+    	expect(fibonacci.getFibonacciNumber(1).to.equal(1));
+    	expect(fibonacci.getFibonacciNumber(2).to.equal(1));
+    	expect(fibonacci.getFibonacciNumber(3).to.equal(2));
+    	expect(fibonacci.getFibonacciNumber(4).to.equal(3));
     });
 });
 
