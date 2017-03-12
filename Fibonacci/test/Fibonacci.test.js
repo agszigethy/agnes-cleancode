9ade380312bbedb959f1e0b930dcdced8438e882
raydecastro <captray108@gmail.com>
2017-03-11 22:05:47 -0800

    feature. added failing getFib# test.

--- Fibonacci/test/Fibonacci.test.js
+++ Fibonacci/test/Fibonacci.test.js
@@ -7,4 +7,11 @@ describe("Fibonacci", () => {
 
         expect(fibonacci).to.be.defined;
     });
+
+    it("shall have the ability to get the fibonacci number by index", () => {
+        let fibonacci = new Fibonacci();
+
+	expect(fibonacci.getFibonacciNumber(0)).to.equal(0);
+    });
 });
+
